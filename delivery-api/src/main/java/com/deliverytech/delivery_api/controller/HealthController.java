4f986fec869c96325a98d2f007974e126857dfa8
pedroo-augustto <pedroamb1992@gmail.com>
2026-05-29 06:32:35 -0300

    Atualizações

---
+++ delivery-api/src/main/java/com/deliverytech/delivery_api/controller/HealthController.java
@@ -0,0 +1,34 @@
+package com.deliverytech.delivery_api.controller;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public Map<String, String> health() {
+       return Map.of(
+            "status", "UP",
+            "timestamp", LocalDateTime.now().toString(),
+            "service", "Delivery API",
+            "javaVersion", System.getProperty("java.version")
+       );
+    }
+
+    @GetMapping("/info")
+    public AppInfo info() {
+        return new AppInfo("Delivery Tech API", "1.0,", "Elaine", "Spring boot");
+    }
+
+    public record AppInfo(
+        String application,
+        String version,
+        String developer,
+        String framework
+    ){}
+
+}
